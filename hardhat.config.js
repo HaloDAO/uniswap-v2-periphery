82668ffedc1106f5f008671e86ca542f18a3206b
Barry Lavides <lavidesbarry@gmail.com>
2021-04-27 20:54:52 +0800

    Kovan deployment setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,20 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
- require("@nomiclabs/hardhat-waffle");
- module.exports = {
-   solidity: "0.6.6",
- }; 
+require('dotenv').config()
+require("@nomiclabs/hardhat-waffle");
+
+const INFURA_URL = process.env.INFURA_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.6.6",
+  networks: {
+    kovan: {
+      url: INFURA_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
+};
+
+
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,21 @@
+require('dotenv').config()
 async function main() {
     // Get signer
     const [deployer] = await ethers.getSigners();
 
-    let weth;
-    const FACTORY_ADDRESS = '0x648db609d49e9cc626ddba0e588445fd19d2dcc3';
+    let weth, router;
+    const FACTORY_ADDRESS = process.env.FACTORY_ADDRESS;
 
     const WETH = await ethers.getContractFactory('WETH');
     const WETHContract = await WETH.deploy();
     weth = await WETHContract.deployed();
 
     const Router = await ethers.getContractFactory('UniswapV2Router02');
-    await Router.deploy(FACTORY_ADDRESS, weth.address);
+    const RouterContract = await Router.deploy(FACTORY_ADDRESS, weth.address);
+    router = await RouterContract.deployed();
+
+    console.log('WETH Contract Address: ', weth.address);
+    console.log('Router Contract Address: ', router.address);
   }
   
 main()
