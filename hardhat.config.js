a0aa9c5d4e545d97c84804bdf436fab471370236
Barry Lavides <lavidesbarry@gmail.com>
2021-05-17 21:54:21 +0800

    hardhat etherscan auto verify contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,9 +3,11 @@
  */
 require('dotenv').config()
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 const INFURA_URL = process.env.INFURA_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.6.6",
@@ -14,6 +16,9 @@ module.exports = {
       url: INFURA_URL,
       accounts: [`0x${PRIVATE_KEY}`]
     }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
   }
 };
 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,29 @@
 require('dotenv').config()
+const hre = require('hardhat')
+
 async function main() {
     // Get signer
     const [deployer] = await ethers.getSigners();
 
-    let weth, router;
+    let router;
     const FACTORY_ADDRESS = process.env.FACTORY_ADDRESS;
-
-    const WETH = await ethers.getContractFactory('WETH');
-    const WETHContract = await WETH.deploy();
-    weth = await WETHContract.deployed();
+    const WETH_ADDRESS = process.env.WETH_ADDRESS;
 
     const Router = await ethers.getContractFactory('UniswapV2Router02');
-    const RouterContract = await Router.deploy(FACTORY_ADDRESS, weth.address);
+    const RouterContract = await Router.deploy(FACTORY_ADDRESS, WETH_ADDRESS);
     router = await RouterContract.deployed();
 
-    console.log('WETH Contract Address: ', weth.address);
+    console.log('WETH Contract Address: ', WETH_ADDRESS);
     console.log('Router Contract Address: ', router.address);
+
+    console.log('Verifying Factory Contract');
+    await hre.run('verify:verify', {
+      address: router.address,
+      constructorArguments: [
+        FACTORY_ADDRESS,
+        WETH_ADDRESS
+      ]
+    })
   }
   
 main()
